126250107e59fbcacf3a500e3ede996eda5ef6b5
Евгений Волков <vamper.2012@gmail.com>
2020-02-18 10:08:52 +0800

    main

--- app/js/common.js
+++ app/js/common.js
@@ -1,9 +1,91 @@
 document.addEventListener("DOMContentLoaded", ready());
 
+
 function ready() {
 
+    var popup = (function (selector) {
+        return function (selector) {
+            var _trigers = document.querySelectorAll(selector);
+    
+            function popupOpen(elem) {
+                var _modal = document.querySelector(elem);
+    
+                if (_modal.querySelector('.popup__close') != null) {
+                    _modal.querySelector('.popup__close').addEventListener('click', function () {
+                        popupClose(elem);
+                    });
+                }
+    
+                _modal.addEventListener('click', function (e) {
+                    if (e.target.classList.contains('popup')) {
+                        popupClose(elem);                 
+                    }                
+                });
+    
+                _modal.classList.add('popup--open');
+    
+                return true;
+            }
+    
+            function popupClose (elem) {
+                var _modal = document.querySelector(elem);
+    
+                _modal.classList.remove('popup--open');   
+    
+                return true;
+            }
+    
+            for (var i = 0; i < _trigers.length; i++) {
+               _trigers[i].addEventListener('click', function (e) {
+                   e.preventDefault();
+                   var id = this.getAttribute('href');             
+                   
+                   popupOpen(id);                
+               });
+            }
+    
+            return {
+                open(id) {
+                    popupOpen(id);
+                },
+                close(id) {
+                    popupClose(id);
+                }
+            }
+        }
+    }());
+    
+    //Forms
+    function submitHandler(e){    
+        e.preventDefault();
+        var self = this;
+        fetch("mail.php", {
+            method: "POST",
+            body: new FormData(self)
+        }).then(function() {
+            if (document.querySelector('.popup-open') != null) {
+                var id ='#' + document.querySelector('.popup-open').getAttribute('id');       
+                popupContacts.close(id);
+            }   
+            //popupContacts.open("#popup-success-massage");
+            alert('Спасибо, скоро наши специалисты свяжутся с вами');
+            self.reset();
+        })
+        .catch(function(error) { console.log(error); });
+    } 
+
+    document.querySelectorAll('.form').forEach(function(element) {
+        element.addEventListener('submit', submitHandler);
+    });
+
+    //Popups
+    if (document.querySelector('.popup-trigger') != null) {
+        popupContacts = popup('.popup-trigger');        
+    }
+
     //Scroll to anchor
-    var linkNav = document.querySelectorAll('[href^="#"]'),
+    try {
+        var linkNav = document.querySelectorAll('[href^="#"]'),
         speed = 0.2; 
     
         for (var i = 0; i < linkNav.length; i++) {
@@ -12,6 +94,11 @@ function ready() {
             
             var w = window.pageYOffset,
                 hash = this.href.replace(/[^#]*(.*)/, '$1');
+
+            if (hash == '#') {
+                return false;
+            }
+                
             t = document.querySelector(hash).getBoundingClientRect().top,
                 start = null;
             requestAnimationFrame(step);
@@ -28,6 +115,9 @@ function ready() {
                 }
             }
         }, false);
+    }
+    } catch (error) {
+        console.log(error);
     }
 
     //Menu cleaveth
@@ -52,7 +142,11 @@ function ready() {
         perView: 1
     });
 
-    stocksSlider.mount();
+    var aboutSlider = new Glide('.about__slider', {
+        type: 'carousel',
+        animationDuration: 1000,
+        perView: 1
+    });
 
     var newsSlider = new Glide('.news__slider', {
         type: 'slider',
@@ -71,14 +165,28 @@ function ready() {
           }
     });
 
+    document.querySelector('.about__arrow--prev').addEventListener('click', function () {
+        aboutSlider.go('<');
+    });
+
+    document.querySelector('.about__arrow--next').addEventListener('click', function () {
+        aboutSlider.go('>');
+    });
+
+    stocksSlider.mount();
+    aboutSlider.mount();
     newsSlider.mount();
 
+    
+
     //Mobile menu
+    var menu = document.querySelector('.menu');
+    var menuActiveClass = 'menu--active';
+    var buttonActiveClass = 'menu__button--active';
 
     document.querySelector('.menu__button').addEventListener('click', function() {
-        var menu = document.querySelector('.menu');
-        var menuActiveClass = 'menu--active';
-        var buttonActiveClass = 'menu__button--active';
+        
+        
 
         if (menu.classList.contains(menuActiveClass)) {
             menu.classList.remove(menuActiveClass);
@@ -89,6 +197,14 @@ function ready() {
         }
     });
 
+    document.querySelector('.menu__list').addEventListener('click', function(e) {   
+        if (menu.classList.contains(menuActiveClass)) {
+            console.log('test')
+            menu.classList.remove(menuActiveClass);
+            document.querySelector('.menu__button').classList.remove(buttonActiveClass);
+        }
+    });
+
 }
 
-
+var popupContacts;
